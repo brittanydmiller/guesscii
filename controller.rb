3f6a2b6fe2b62e596fdee2f627d83e746d911823
& <pair+anfruth.brittanydmiller@devbootcamp.com>
2014-04-17 18:45:31 -0700

    awseomeness sudocode hahahahah Brick

---
+++ controller.rb
@@ -0,0 +1,21 @@
+# send welcome message to view
+# View.welcome (welcome message, instructions, let's get started - make full screen - press any key
+####### View.options
+####### Make a selection of your option - take user input and press enter- compare against model
+# if exit is typed, then stop, otherwise continue
+# assuming it is not exit, then show first two lines of image (View.lines)
+# ask for user input
+# if user input == the answer, you win
+  # if win, show whole picture
+# if user input != the answer, continue guessing  -ask you for user input
+  # if you see all picture, but have not yet guessed it, then say nice try.  YOU ARE GREAT AND SPECIAL!!!!KK!K!!!
+
+class Controller
+
+
+end
+
+
+
+game = Controller.new
+game
