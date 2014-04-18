f5bb2e2e43829cc51547ea03025d9926c7757bbf
& <pair+anfruth.brittanydmiller@devbootcamp.com>
2014-04-18 15:36:15 -0700

    Lisa Frank Madness

--- controller.rb
+++ controller.rb
@@ -4,7 +4,6 @@ require_relative 'view'
 require_relative 'game'
 require_relative 'picture'
 
-
 class Controller
   attr_reader :max_guesses, :game
 
@@ -14,11 +13,10 @@ class Controller
     @logo = PictureManager.new('logo.txt')
     View.welcome(@logo, @max_guesses)
     run_game
-
   end
 
   def run_game
-    case View.get_input.to_i
+    case gets.chomp.to_i
       when 1
         (1..8).to_a.shuffle.each do |index|
           @guess_count = 1
@@ -28,13 +26,20 @@ class Controller
         View.game_over(@logo, @max_guesses, @total_score)
       when 2
         View.instructions
+        return_to_menu
       when 3
         View.game_info
+        return_to_menu
+      when 4
+        View.game_over(@logo, @max_guesses, @total_score)
     end
   end
 
-
-
+  def return_to_menu
+    View.enter
+    View.welcome(@logo, @max_guesses) if gets.chomp
+    run_game
+  end
 
   def setup_game(index)
     @picture = Picture.find(index)
@@ -45,21 +50,36 @@ class Controller
   def play_game
     View.image_piece(@parser, @guess_count, @max_guesses)
     guess = get_user_guess
-    if check_user_response(guess)
-      View.win(@parser, @max_guesses)
-      View.score(@game.score)
-      @total_score += @game.score
-      View.total_score(@total_score)
-    elsif @guess_count > @max_guesses + 1
-      View.lose(@parser, @max_guesses, @picture.answer)
+    if guess == "exit"
+      View.game_over(@logo, @max_guesses, @total_score)
+      exit
+    elsif check_user_response(guess)                      #correct guess
+      user_win(guess)
+    elsif @guess_count > @max_guesses + 1              #user loses
+      user_lose
     else
-      @game.reduce_score
-      if @game.score == 0
-        View.score(@game.score)
-        View.total_score(@total_score)
-      else
-        play_game
-      end
+      continues_guessing                               #user continues, outcome uncertain
+    end
+  end
+
+  def user_win(guess)
+    View.win(@parser, @max_guesses)
+    View.score(@game.score)
+    @total_score += @game.score
+    View.total_score(@total_score)
+  end
+
+  def user_lose
+    View.lose(@parser, @max_guesses, @picture.answer)
+  end
+
+  def continues_guessing
+    @game.reduce_score
+    if @game.score == 0
+      View.score(@game.score)
+      View.total_score(@total_score)
+    else
+      play_game
     end
   end
 
@@ -71,8 +91,4 @@ class Controller
   def check_user_response(guess)
     @game.check_answer(guess)
   end
-
 end
-
-
-controller = Controller.new
